0d9ed8e01a1489957b2a1114dc04765b599b222f
Terry Dellino <terry.dellino@gmail.com>
2021-11-06 10:49:15 -0400

    Make ll alias more compact

--- zsh/.config/zsh/alias.zsh
+++ zsh/.config/zsh/alias.zsh
@@ -2,7 +2,7 @@
 if type exa > /dev/null; then
   alias ls='exa --classify --group-directories-first'
   alias lx='exa --oneline --classify --all --group-directories-first'
-  alias ll='exa --long --classify --all --group-directories-first'
+  alias ll='exa --classify --all --group-directories-first'
   alias lxt='exa --icons --tree --level'
   alias lxt2='exa --icons --tree --level 2'
 fi
