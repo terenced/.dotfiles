2aeab93499cdcdb4a1bfbf0c3fe7d1bd8fcbc6a4
Terry Dellino <terry.dellino@lever.co>
2023-02-02 13:15:33 -0500

    Load starship theme

--- zsh/.config/zsh/theme.zsh
+++ zsh/.config/zsh/theme.zsh
@@ -1,12 +1,14 @@
 zplug "mafredri/zsh-async", from:github
-
-if [[ "$(uname)" = "Darwin" ]] then
-  zplug "denysdovhan/spaceship-prompt", use:spaceship.zsh, from:github, as:theme
-  # eval "$(starship init zsh)"
-  # export STARSHIP_CONFIG=~/.starship/config.toml
-else
-  zplug "iboyperson/pastel", as:theme
-fi
+zplug "axieax/zsh-starship", from:github
+#
+# #  zplug "iboyperson/pastel", as:theme
+# if [[ "$(uname)" = "Darwin" ]] then
+#   zplug "denysdovhan/spaceship-prompt", use:spaceship.zsh, from:github, as:theme
+#   # eval "$(starship init zsh)"
+#   # export STARSHIP_CONFIG=~/.starship/config.toml
+# else
+#   zplug "iboyperson/pastel", as:theme
+# fi
 
 #zplug "romkatv/powerlevel10k", as:theme, depth:1
 # To customize prompt, run `p10k configure` or edit ~/.config/zsh/.p10k.zsh.
