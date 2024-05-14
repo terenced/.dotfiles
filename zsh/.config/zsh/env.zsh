4aa9976a46651b00638358fa10ff4c153e4f027f
Terry Dellino <terry.dellino@gmail.com>
2024-05-14 09:59:08 -0400

    feat(zsh): clean up utils

--- zsh/.config/zsh/env.zsh
+++ zsh/.config/zsh/env.zsh
@@ -26,5 +26,11 @@ fi
 # source "$HOME/.cargo/env"
 export PATH=~/.local/bin:$PATH
 
+export FZF_DEFAULT_OPTS=" \
+--height=40% --layout=reverse --info=inline --border --margin=1 --padding=1 \
+--color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284 \
+--color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf \
+--color=marker:#f2d5cf,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284"
+
 # export DENO_INSTALL="/home/terenced/.deno"
 # export PATH="$DENO_INSTALL/bin:$PATH"
--- zsh/.config/zsh/utils.zsh
+++ zsh/.config/zsh/utils.zsh
@@ -1,5 +1,5 @@
-# fbd - delete git branch (including remote branches)
-fbd() {
+# Delete branches with fzf
+gbdf() {
   local branches branch
   branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
   branch=$(echo "$branches" | fzf --multi --height=40% --layout=reverse --info=inline --border --margin=1 --padding=1 ) &&
